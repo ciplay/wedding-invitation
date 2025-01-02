05278134a7113347a171f9e299e330c2d0e4cfca
Risky Satria <riskysatria666@gmail.com>
2025-01-02 10:26:16 +0000

    Resolving error deployment

--- app/[guestName]/page.tsx
+++
@@ -1,6 +0,0 @@
-import WeddingInvitation from '@/components/wedding-invitation'
-
-export default function GuestInvitationPage({ params }: { params: { guestName: string } }) {
-  return <WeddingInvitation guestName={params.guestName} />
-}
-
